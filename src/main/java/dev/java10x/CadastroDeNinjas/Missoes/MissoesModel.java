7956248e240043636462db463c95ef6047fded7e
joseeneto19 <josemnetodev@gmail.com>
2025-07-14 14:14:58 -0300

    feat: Adiciona o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity // Ele transforma uma classe em uma entidade do BD
 @Table(name = "tb_missoes") // Gerar uma tabela no BD
+@Data // Lombok cria getters e setters
+@NoArgsConstructor // Lombok cria um construtor sem argumentos
+@AllArgsConstructor // Lombok cria um construtor com todos os argumentos
 public class MissoesModel {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //Falar que o atributo é um ID // Gerar ID automaticamente
@@ -16,4 +22,5 @@ public class MissoesModel {
 
     @OneToMany (mappedBy = "missoes") // Uma missão para muitos ninjas
     private List<NinjaModel> ninja;
+
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity // Ele transforma uma classe em uma entidade do BD
 @Table(name = "tb_cadastro") // Gerar uma tabela no BD
+@Data // Lombok cria getters e setters
+@NoArgsConstructor // Lombok cria um construtor sem argumentos
+@AllArgsConstructor // Lombok cria um construtor com todos os argumentos
 public class NinjaModel {
 
     @Id  @GeneratedValue(strategy = GenerationType.IDENTITY) //Falar que o atributo é um ID // Gerar ID automaticamente
@@ -16,38 +22,4 @@ public class NinjaModel {
     @ManyToOne //ManyToOne Um ninja tem uma unica missao
     @JoinColumn(name = "missoes_id") // Foreing Key ou chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
